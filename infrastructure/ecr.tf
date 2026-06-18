b0c946d3ffac01b35c46ea923c577972d99cb8e3
Odon Nkongolo <odonnkongolo@gmail.com>
2026-06-18 18:01:51 +0100

    fix: snyk errors and recommendations to patch vulnerabilities

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -1,5 +1,6 @@
 resource "aws_ecr_repository" "fintech_ledger_repo" {
   name                 = "fintech-ledger-app"
+  force_delete         = true
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
